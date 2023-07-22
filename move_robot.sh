36b522da3e73f9843396cec5d16910404547bcf9
feuerschlucker <hroethl@gmail.com>
2023-07-22 18:26:52 +0200

    had my fun today

--- move_robot.sh
+++ move_robot.sh
@@ -1,2 +1,4 @@
+!#/bin/sh
+
 ign topic -t "/cmd_vel" -m ignition.msgs.Twist -p "linear: {x: 0.5}, angular: {z: 0.00}"
 
--- src/data_generation/world_generation.py
+++ src/data_generation/world_generation.py
@@ -28,15 +28,18 @@ def generate_tiles(template_path: str) -> str:
          
     for i in range(10):
         #range 4 to 6 updates only the middle tiles, which are used in carworld5
-        for j in range(4,6):
-            mname = "tile_"+str(i-5)+"_"+str(j-5)
-            pos = str(-9+2*i)+" "+str(-9+2*j)+" 0 0 0 0"           
-            #m1=str(1)
-            m1= str(round(random.random(),2)*100)
-            #m2=str(1)
-            m2 = str(round(random.random(),2)*100)
+        for j in range(2):
+            mname = "tile_"+str(i)+"_"+str(j)
+            pos = str(-9+2*i)+" "+str(j-0.5)+" 0 0 0 0"           
+            m1=str(1)
+            #m1= str(round(random.random(),2)*50+50)
+            m2=str(1)
+            if (j==1):
+                m2=str(1)
+            
+            #m2 = str(round(random.random(),2)*0.5)
 
-            if(float(m1)<50):
+            if(float(m2)<1):
                 color ="0.9 0.1 0"
             else:
                 color ="0.1 0.9 0"
@@ -66,27 +69,38 @@ def generate_objects(template_path: str) -> str:
     color3 ='0 0 1 1'
     
     # generate 16 objects
-    for i in range(16):
+    no_obj = 20
+    posx = -10
+    increment = 20 /no_obj
+    print(increment)
+    for i in range(no_obj):
+   
         # replacement for [[OBJECT]]
         obj_name = "cyl_"+str(i)
      
         # replacement for [[POSITION]]
         # first objects set in X direction starting from -3 as robot has 
         # limited side vision
-        posx= str(round(random.uniform(-3,10),2))
-        posy= (round(random.uniform(-7,7),2))
-        while (posy >-2 and posy <2):
-            posy= (round(random.uniform(-7,7),2))
-
-        posy = str(posy)
-        position = posx+" "+posy+" 0.5 0 -0 0"
+        #posx= str(round(random.uniform(-3,10),2))
         
+        posx = posx + increment + random.uniform(-1,1)
+        posxx = str(round(posx,2))
+   
+        if i%2 ==0:
+            posy = str(-2.5)
+        else:
+            posy= str(2.5)
+
         # replacement for [[RADIUS]]
         radius = str(round(random.uniform(0.2,1),2))
 
         # replacement for [[LENGTH]]
-        length = str(round(random.uniform(0.2,3),2))
-
+        length = round(random.uniform(0.2,3),2)
+        posz = str(round(length/2,2))
+        length = str(length)
+        
+        position = posxx+" "+posy+" "+posz+" 0 -0 0"
+        
         # replacement for [[COLOR]]
         selector = random.randint(1,3)
 
@@ -110,14 +124,22 @@ def generate_objects(template_path: str) -> str:
 
 def main():
     generate_world(
-        main_file="gazebo_templates/main_base1.sdf",
+        main_file="gazebo_templates/main_base.sdf",
         output_path="gazebo_generated/generated_world.sdf",
         replace_map={
             "[[STEP_SIZE]]": str(0.1),
-            "[[TILES]]": generate_tiles("gazebo_templates/tile_base.sdf"),
+            "[[TILES]]": "",
             "[[CYLINDERS]]": generate_objects("gazebo_templates/cylinder_base.sdf"),
             "[[ROBOT]]": read_file("gazebo_templates/robot_stacked_drive.sdf")
         },
+# =============================================================================
+#         replace_map={
+#             "[[STEP_SIZE]]": str(0.1),
+#             "[[TILES]]": generate_tiles("gazebo_templates/tile_base_ori.sdf"),
+#             "[[CYLINDERS]]": generate_objects("gazebo_templates/cylinder_base.sdf"),
+#             "[[ROBOT]]": read_file("gazebo_templates/robot_stacked_drive.sdf")
+#         },
+# =============================================================================
     )
 
 if __name__ == "__main__":
